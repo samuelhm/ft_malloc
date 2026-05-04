c9583ff9e7ff10e3e1f1deec201dde086cef0b89
Samuel Hurtado Marin <shurtado@student.42barcelona.com>
2026-05-04 12:22:19 +0200

    planning malloc

--- src/malloc.c
+++ src/malloc.c
@@ -2,9 +2,47 @@
 
 static heap_t g_heap = {0};
 
+void	*get_free_zone(zone_type type, size_t size)
+{
+		zone_t *zone = (type == TINY) ? g_heap.tiny_zones : g_heap.small_zones;
+		while (zone != NULL)
+		{
+			block_t *block = find_free_block(zone, size);
+			if (block != NULL)
+				return (split_block(block, size));
+			zone = zone->next;
+		}
+		zone = create_zone(type, size);
+		if (zone == NULL)
+			return (NULL);
+		zone->next = (type == TINY) ? g_heap.tiny_zones : g_heap.small_zones;
+		if (type == TINY)
+			g_heap.tiny_zones = zone;
+		else
+			g_heap.small_zones = zone;
+		return (split_block(zone->first_block, size));
+}
+
 void	*malloc(size_t size)
 {
 	if (size <= 0)
 		return (NULL);
-	return (NULL);
+	zone_type type = get_zone_type(size);
+	block_t *block;
+
+	if (type == LARGE)
+	{
+		zone_t *zone = create_zone(LARGE, size);
+		if (zone == NULL)
+			return (NULL);
+		return (void *)((char *)zone + ZONE_META_SIZE);
+	}
+	else
+	{
+		block = get_free_zone(type, size);
+		if (block == NULL)
+			return (NULL);
+	}
+	return ((void *)((char *)block + BLOCK_META_SIZE));
+
 }
